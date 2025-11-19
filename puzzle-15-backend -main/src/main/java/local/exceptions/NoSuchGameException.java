51f6f0ec15d611652ff0bdea4e993ae4e3ac509a
Shehryar Raja <rshehryar29@gmail.com>
2025-11-19 16:25:42 +0100

    Delete puzzle-15-backend -main directory

--- puzzle-15-backend -main/src/main/java/local/exceptions/NoSuchGameException.java
+++
@@ -1,7 +0,0 @@
-package local.exceptions;
-
-public class NoSuchGameException extends RuntimeException {
-    public NoSuchGameException(String message) {
-        super(message);
-    }
-}
--- puzzle-15-backend -main/src/main/java/local/game/Direction.java
+++
@@ -1,5 +0,0 @@
-package local.game;
-
-public enum Direction {
-    UP, DOWN, LEFT, RIGHT
-}
